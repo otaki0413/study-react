5503d76753b304c1d2a1af4932a667f89c7cbbd4
otaki0413 <takida0413@gmail.com>
2022-10-31 02:30:58 +0900

    ♻️ export defaultの削除

--- components/Headline.js
+++ components/Headline.js
@@ -1,15 +1,14 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-export default function Headline(props) {
-
+export function Headline(props) {
   return (
     <div>
       <h1 className={styles.title}>{props.page} page</h1>
 
       <p className={styles.description}>
-        Get started by editing{' '}
+        Get started by editing{" "}
         <code className={styles.code}>pages/{props.page}.js</code>
       </p>
     </div>
-  )
+  );
 }
