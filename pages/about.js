284d1613cec3eb920de28b3da5022c22bf8c87e0
otaki0413 <takida0413@gmail.com>
2022-10-31 03:17:57 +0900

    🐛  メインコンポーネントインポート時の{ }を追加

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
-import Main from "../components/Main";
+import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
