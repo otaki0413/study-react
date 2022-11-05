d936db850615b7f81bf66d3ee78f130f1630fc8c
otaki0413 <takida0413@gmail.com>
2022-11-05 11:00:48 +0900

    ⚗️ 不要な記述でいつか消しますが、クリックイベントを実装してみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,38 @@ import styles from "src/styles/Home.module.css";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
+import { useCallback } from "react";
+import Link from "next/link";
+
+// コンポーネントの外にメソッドを書く場合
+// const handleClick = (e) => {
+//   console.log(e.target.value);
+//   e.preventDefault();
+//   alert(foo);
+// };
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.value);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
-      <Header/>
+      <Header />
+      <Link
+        href='/about'
+        onClick={handleClick}
+        legacyBehavior
+      >
+        <a>ボタン</a>
+      </Link>
       <Main page='index' />
       <Footer />
     </div>
